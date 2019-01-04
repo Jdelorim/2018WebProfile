01dd18ff7e8a6d791a1ae2358510e7c1549e0b6b
Joshua deLorimier <TRASHTRASH_HQ@Joshuas-Mac-Pro.localdomain>
2019-01-04 02:51:28 -0500

    started portfolio

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -2,9 +2,12 @@ document.addEventListener("DOMContentLoaded", () => {
     var plax = document.querySelectorAll('.parallax');
     var scrollspy = document.querySelectorAll('.scrollspy');
     var sideNav = document.querySelectorAll('.sidenav');
-
+    var carousel = document.querySelectorAll('.carousel');
+    var tooltiper = document.querySelectorAll('.tooltipped');
+     
+    M.Tooltip.init(tooltiper, {});
+    M.Carousel.init(carousel, {});
     M.Sidenav.init(sideNav, {});
-    
     M.ScrollSpy.init(scrollspy,{
         scrollOffset: 65
     });
