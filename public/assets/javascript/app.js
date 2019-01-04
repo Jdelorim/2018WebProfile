df27d0f2eba31f6b17217d634df8abfddfa7366e
Joshua deLorimier <TRASHTRASH_HQ@Joshuas-Mac-Pro.localdomain>
2019-01-04 01:41:03 -0500

    added hamburger and mobile responsivness

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -1,11 +1,12 @@
-console.log("helloworld");
-
 document.addEventListener("DOMContentLoaded", () => {
-    var plax = document.querySelectorAll(".parallax");
+    var plax = document.querySelectorAll('.parallax');
     var scrollspy = document.querySelectorAll('.scrollspy');
+    var sideNav = document.querySelectorAll('.sidenav');
+
+    M.Sidenav.init(sideNav, {});
     
     M.ScrollSpy.init(scrollspy,{
-        scrollOffset: 70
+        scrollOffset: 65
     });
 
      M.Parallax.init(plax,{
